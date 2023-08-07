f8b4199a2ae36ac13a289a1d0c7bbef64a1ad7e7
ugljik <ugljik@kali.kali>
2023-08-07 22:38:20 +0200

    added storage for images

--- app/AddPost.tsx
+++ app/AddPost.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { makePost } from "@/lib/makePost";
+import { makeImage, makePost } from "@/lib/makePost";
 import { useState } from "react";
 
 export default function AddPost() {
@@ -11,8 +11,8 @@ export default function AddPost() {
 
   async function handleSubmit(e: any) {
     e.preventDefault();
-    title && post && condensed
-      ? await makePost(title, condensed, post)
+    title && post && condensed && image
+      ? (await makePost(title, condensed, post, image.name), makeImage(image))
       : alert("Please fill in all inputs.");
   }
   return (
@@ -50,9 +50,9 @@ export default function AddPost() {
         />
         <button type="submit">Add post</button>
       </form>
-      <div className="flex flex-col basis-1/3 border rounded-2xl p-4 overflow-scroll">
-        <h1 className="text-center font-bold text-xl">Preview: </h1>
-        <img src={image} />
+      <div className="flex flex-col basis-1/3 border rounded-2xl p-4 overflow break-words ">
+        <h1 className="text-center font-bold te xt-xl">Preview: </h1>
+        {image && <img src={URL.createObjectURL(image)} className="w-32" />}
         <h2>{title}</h2>
         <h4>{condensed}</h4>
         <p>{post}</p>
--- lib/makePost.ts
+++ lib/makePost.ts
@@ -1,12 +1,26 @@
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 import { app } from "./firebase";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const db: any = getFirestore(app);
 
-export async function makePost(title: string, condensed: string, post: string) {
+export async function makePost(
+  title: string,
+  condensed: string,
+  post: string,
+  imageName: string
+) {
   await addDoc(collection(db, "blogpost"), {
     title: title,
     condensed: condensed,
     post: post,
+    imageName: imageName,
   });
+}
+
+const storage: any = getStorage(app);
+
+export async function makeImage(image: any) {
+  const storageRef: any = ref(storage, `images/${image.name}`);
+  uploadBytes(storageRef, image);
 }
